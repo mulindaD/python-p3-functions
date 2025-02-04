54e1cec5e839763d0865f5b39a6432f802cab2ae
mulindaD <mulindarweshumba@gmail.com>
2025-02-04 10:45:16 +0300

    Python funcs

--- lib/functions.py
+++ lib/functions.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    print('Hello, programmer!')
+    print("Hello, programmer!")
 
 def greet(name):
     print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
+def greet_with_default(name = "programmer"):
     print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
 
-def halve(number):
-    if not isinstance(number, (int, float)):
-        return None
-    return number / 2
+def halve(num):
+    return num/2
+
+
--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-from functions import greet_programmer, greet, greet_with_default, \
-                        add, halve
-
+from functions import greet_programmer, greet, greet_with_default, add, halve
 import io
 import sys
 
